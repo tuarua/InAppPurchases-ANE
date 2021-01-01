5e137934c082ed711287d172380d895918e749b0
Eoin Landy <info@tuarua.co.uk>
2021-01-01 13:21:44 +0000

    1.7.0

--- native_extension/ane/build.sh
+++ native_extension/ane/build.sh
@@ -9,7 +9,7 @@ PROJECTNAME=InAppPurchaseANE
 fwSuffix="_FW"
 libSuffix="_LIB"
 
-AIR_SDK="/Users/eoinlandy/SDKs/AIRSDK_33.1.1.300"
+AIR_SDK="/Users/eoinlandy/SDKs/AIRSDK_33.1.1.345"
 
 ##############################################################################
 
